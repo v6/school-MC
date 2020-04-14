d268601495fc3c6fb712de80ea801596c710acf2
Nathan (git.io/v6) <5750764+v6@users.noreply.github.com>
2020-04-14 01:18:54 -0700

    thereifixedit

--- lab2.sh
+++ lab2.sh
@@ -13,15 +13,28 @@ inputvar=0
 biggestvar=0
 continuevar="yes"
 
+## Part 1: Get User Input, make comparisons
 
 until [ $continuevar == "no" ] 
 do
+	##  We'll repeatedly get number input from the user,
+	##  and update the biggest number.
+	##  If the user doesn't want to continue, we'll
+	##  exit the loop.
         echo "Enter the number, to escape enter a string"
+	echo ""
         read inputvar
         if [ $inputvar -gt $biggestvar ]
-        then $biggestvar=$inputvar
+        then biggestvar=$inputvar
         else echo "The number you entered isn't the biggest."
         fi
         echo "Do you want to continue?"
         read continuevar
 done
+
+##  Part 2: Give the output to the user upon exiting.
+##  By this point, $biggestvar should be the largest
+##  number, because we've been updating it each time.
+echo ""
+echo "The biggest number, of the ones you entered, was"
+echo $biggestvar
